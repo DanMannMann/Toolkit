cf78241579e4e7284f25cbd8b6bf33ef6c908019
DanMannMann <dan@marsman.co.uk>
2015-10-18 13:16:35 +0100

    add dispatcher schedule extension

---
+++ Marsman.Toolkit/WpfExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Marsman.Toolkit
+{
+    public static class WpfExtensions
+    {
+        public static void Schedule(this Dispatcher dispatcher, TimeSpan schedule, Action action)
+        {
+            DispatcherTimer dt = new DispatcherTimer(
+                schedule, 
+                DispatcherPriority.Normal, 
+                new EventHandler((o, e) =>
+                {
+                    (o as DispatcherTimer).Stop();
+                    action.Invoke();
+                }),
+                dispatcher);
+            dt.Start();
+        }
+    }
+}
